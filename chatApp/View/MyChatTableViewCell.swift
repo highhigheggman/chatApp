51811c503fe562e9fe48ccf1165413876a4a6e52
highhigheggman <highhigheggman@yahoo.co.jp>
2018-12-08 20:24:02 +0900

    UI [12/8: 2時間ほど]

---
+++ chatApp/View/MyChatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyChatTableViewCell.swift
+//  chatApp
+//
+//  Created by yoshiki-t on 2018/12/08.
+//  Copyright © 2018 yoshiki-t. All rights reserved.
+//
+
+import UIKit
+
+class MyChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
