1209f9b3b93a2c51bf9116915a96c2455fbcd65b
highhigheggman <highhigheggman@yahoo.co.jp>
2018-12-26 00:50:24 +0900

    Account管理クラスの作成 [12/26 10分]【累計: 14時間15分】

---
+++ chatApp/Class/AccountManager.swift
@@ -0,0 +1,34 @@
+//
+//  AccountManager.swift
+//  chatApp
+//
+//  Created by yoshiki-t on 2018/12/26.
+//  Copyright © 2018 yoshiki-t. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+protocol AccountManagerProtocol {
+    static var `default` : AccountManagerProtocol { get }
+    
+    func setUserModel(userModel: UserModel)
+    var user: UserModel? { get }
+}
+
+class AccountManager {
+    
+    private var userModel: UserModel?
+    
+    static var `default` : AccountManager = {
+        return AccountManager()
+    }()
+    
+    func setUserModel(userModel: UserModel) {
+        self.userModel = userModel
+    }
+    
+    var user: UserModel? {
+        return self.userModel
+    }
+}
